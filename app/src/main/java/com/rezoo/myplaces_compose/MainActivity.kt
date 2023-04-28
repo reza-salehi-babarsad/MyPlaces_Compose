26df730b2926cea91b1c4a847b2ef627ea06ac95
reza <rezasalehibabarsad@hotmail.com>
2023-04-28 12:20:56 +0330

    Initial commit

---
+++ app/src/main/java/com/rezoo/myplaces_compose/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rezoo.myplaces_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rezoo.myplaces_compose.ui.theme.MyPlaces_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyPlaces_ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyPlaces_ComposeTheme {
+        Greeting("Android")
+    }
+}
